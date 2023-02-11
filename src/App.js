8266647dc02ba8f2e00e215558d34228fa9240c6
unicornbug <veronika.djurica@outlook.com>
2023-02-11 11:58:26 +0000

    added date class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <header class="container">
+        <div class="date">Monday </div>
+        <div class="date">13:02 </div>
+        <div class="date">16/2/2017 </div>
+      </header>
+    </div>
+  );
 }
